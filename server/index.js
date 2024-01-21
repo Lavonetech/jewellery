a746d1d5489303e74419ec03cf35e492e23c5a1f
Pradeep Dilruksha <108401187+Lavonetech@users.noreply.github.com>
2024-01-22 00:49:51 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -18,11 +18,11 @@ const orderRouter=require('./routers/orderRoutes');
 app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, '../maera/build')));
 
   app.get('*', (req, res) =>
     res.sendFile(
-      path.resolve(__dirname, '../', 'client', 'build', 'index.html')
+      path.resolve(__dirname, '../', 'maera', 'build', 'index.html')
     )
   );
  
